1880602423894f65cc788f17a15b733a2a36c74a
cgsenor <115461351+cgsenor@users.noreply.github.com>
2023-10-26 23:30:30 +0200

    Con errores de búsquedas

---
+++ src/errores/Errores.java
@@ -0,0 +1,32 @@
+package errores;
+
+/**
+ *
+ * @author gsenorans
+ */
+public class Errores {
+
+	public String errorDniEmpleados() {
+		StringBuilder errorDniEmpleados = new StringBuilder();
+		errorDniEmpleados.append("\nEl Dni no existe en el registro");
+		return errorDniEmpleados.toString();
+	}
+
+	public String errorDepartamentEmpleados() {
+		StringBuilder errorDpartamentEmpleados = new StringBuilder();
+		errorDpartamentEmpleados.append("\nEl departamento no existe en el registro");
+		return errorDpartamentEmpleados.toString();
+	}
+
+	public String errorNomEmpresaEmpleados() {
+		StringBuilder errorDniEmpleados = new StringBuilder();
+		errorDniEmpleados.append("\nEl nombre de la empresa no existe en el registro");
+		return errorDniEmpleados.toString();
+	}
+	
+	public String erroCodicardEmpleados() {
+		StringBuilder errorCodicardEmpleados = new StringBuilder();
+		errorCodicardEmpleados.append("\nEl codigo de tarjeta no existe en el registro");
+		return errorCodicardEmpleados.toString();
+	}
+}
